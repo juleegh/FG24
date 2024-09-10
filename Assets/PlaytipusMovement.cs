7f0c7c0f5cd22780ff0746d12da4cf83aa0de961
juleegh <julymontes495@gmail.com>
2024-09-10 10:55:57 +0200

    Created new character with moving, jumping and punching

---
+++ Assets/PlaytipusMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaytipusMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    private float moveSpeed = 5;
+    private float punchingForce = 100;
+    private float jumpingForce = 600;
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime);
+        transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RaycastHit hitResult;
+            bool hit = Physics.Raycast(transform.position, transform.forward, out hitResult, 3f);
+            if (hit)
+            {
+                hitResult.rigidbody.AddForce(transform.forward * punchingForce);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.X) && IsTouchingFloor() == true)
+        {
+            rb.AddForce(Vector3.up * jumpingForce);
+        }
+
+    }
+
+    private bool IsTouchingFloor()
+    {
+        Vector3 position = transform.position + Vector3.down * 0.5f;
+        RaycastHit hit;
+        return Physics.SphereCast(transform.position, 0.5f, -transform.up, out hit, 1f);
+    }
+}
